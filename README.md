33df4c138b25d62d3eabdf925b1d3e487bb52454
Juliani Schlickmann Damasceno <juliani.schlickmanndamasceno@zapier.com>
2023-09-20 19:44:35 -0700

    Init project

---
+++ README.md
@@ -0,0 +1,3 @@
+# ToDo App
+
+Simple ToDo list app to keep track of tasks.
---
+++ src/components/Header.tsx
@@ -0,0 +1,10 @@
+import logo from "../assets/Logo.svg";
+import styles from "./Header.module.css";
+
+export function Header() {
+  return (
+    <div className={styles.container}>
+      <img src={logo} alt="ToDo App" />
+    </div>
+  );
+}
