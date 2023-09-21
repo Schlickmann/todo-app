4401e85498130f2407fa51dd0f994013a0fe434d
Juliani Schlickmann Damasceno <juliani.schlickmanndamasceno@zapier.com>
2023-09-20 20:11:33 -0700

    Add Form component

---
+++ src/components/Form.tsx
@@ -0,0 +1,33 @@
+import { FormEvent, useState } from "react";
+import { BsPlusCircle } from "react-icons/bs";
+import styles from "./Form.module.css";
+
+interface FormProps {
+  onSubmit: (task: string) => void;
+}
+
+export function Form({ onSubmit }: FormProps) {
+  const [task, setTask] = useState("");
+
+  const handleFormSubmission = (e: FormEvent) => {
+    e.preventDefault();
+
+    // Send new todo to parent component
+    onSubmit(task);
+    setTask("");
+  };
+  return (
+    <form className={styles.container} onSubmit={handleFormSubmission}>
+      <input
+        type="text"
+        placeholder="Add Todo"
+        value={task}
+        onChange={(e) => setTask(e.target.value)}
+      />
+
+      <button type="submit">
+        Add <BsPlusCircle />
+      </button>
+    </form>
+  );
+}
